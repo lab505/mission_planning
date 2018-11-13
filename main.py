bde4b541ecf2e7ce5eba6c8655d15b2ddb004cbb
cuijialiang@byted <cuijialiang@bytedance.com>
2018-11-13 17:10:47 +0800

    add some details

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ class Mission_Planning(object):
     def __init__(self):
         pass
     
+    '''
+    输入
+    mission_type: in ['large_scale', 'midium_scale', 'small_scale']
+    mission_level: in ['science_experiment', 'normal_mission', 'urgent_mission']
+    weather_condition: in ['rainy', 'sunny', ......]
+    wind: in [1,2,3,4]
+    sensor: in ['optical', 'nir', ...]
+    begin_time: like '2018-11-13-14-00'
+    end_time
+    '''
     def create_mission(self, input_):
         assert isinstance(input_, str)
         input_ = json.loads(input_)
