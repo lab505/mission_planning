65bb97801a69351cb11f806eadd8195eb69a5ded
cuijialiang@byted <cuijialiang@bytedance.com>
2018-11-14 15:11:43 +0800

    add some details

--- main.py
+++ main.py
@@ -1,36 +1,92 @@
-import json
+import json, logging
 
+'''
+第四课题任务规划软件的交互模块
+可以用jythonc在java中调用 https://wiki.python.org/jython/LearningJython
+'''
 class Mission_Planning(object):
     def __init__(self):
         pass
     
     '''
+    由应用方/操作人员调用
     输入
+    name: 任务名,字符类型
     mission_type: in ['large_scale', 'midium_scale', 'small_scale']
     mission_level: in ['science_experiment', 'normal_mission', 'urgent_mission']
+    mission_area: 任务地理区域, [(x1,y1),(x2,y2)...] 一个由list表示的多边形
     weather_condition: in ['rainy', 'sunny', ......]
     wind: in [1,2,3,4]
     sensor: in ['optical', 'nir', ...]
+    platform: in ['multi-rotors', 'fixed-wing', ...]
+    flying_height: [min_height, max_height]
+    flying_speed: [min_speed, max_speed]
+    shooting_space_seconds: [min_space, max_space]
     begin_time: like '2018-11-13-14-00'
-    end_time
+    end_time: like '2018-11-13-14-00'
+    返回
+    任务创建的结果:成功or失败
     '''
     def create_mission(self, input_):
-        assert isinstance(input_, str)
-        input_ = json.loads(input_)
-        assert 'name' in input_
+        try:
+            assert isinstance(input_, str)
+            input_ = json.loads(input_)
+            assert 'name' in input_
 
-
-        res = {}
-        res['create_res'] = True
-        res['reply'] = 'create_success'
-        return json.dumps(res)
+            return 1
+        except Exception as e:
+            logging.exception(e)
+            return 0
     
+    '''
+    由应用方/界面调用
+    作为create_mission的补充输入,可以添加资源/地图数据,实时数据等
+    输入
+    json格式的list
+    [data1, data2,...]
+    其中每个data为一个dict,包括字段
+    type: 资源类型
+    resources_data: 具体资源
+    # TODO 细化具体需要哪些data,如地理底图数据等
+    返回
+    添加数据的结果:成功or失败
+    '''
     def add_data(self, input_):
+        try:
+            assert isinstance(input_, str)
+            input_ = json.loads(input_)
+
+            return 1
+        except Exception as e:
+            logging.exception(e)
+            return 0
+    
+    '''
+    由后端程序
+    获取显示状态
+    返回 一个json格式字符串,解析后类型为dict,有以下字段:
+    'title': 主程序标题
+    'mission_type': 界面上的任务类型 等
+    # TODO 明确主界面需要显示的内容
+    '''
+    def get_main_ui_display(self):
         pass
     
-    def get_display(self):
-        pass
+    '''
+    获取任务规划结果 - 可以发给控制中心执行飞行的结果
+    返回值 json格式dict 内容包括:
+    'name':'test_mission', //任务名
+    'mission_type': MissionType, //任务类型(洪涝 or 反恐 or 生态),枚举型
+    'mission_area':[(x1,y1),(x2,y2)...], //要求任务区域:list类型表示的多边形,代表任务区域
+    'platform': Platform, //要求平台:枚举类型
+    'camera': Camera, //要求相机(载荷):枚举类型
+    'begin_time': Unix_time, //任务开始时间, 0即立即开始
     
-    def get_mission_planning_res(self, input_):
-        pass
-        ###########
+    'flying_height': [min_height, max_height], //飞行高度
+    'flying_speed': [min_speed, max_speed], //飞行速度
+    'shooting_space_seconds': [min_space, max_space], //拍摄间隔
+    'track': [(x1,y1),(x2,y2)...], //航迹
+    '''
+    def get_mission_planning_res(self):
+        res = {}
+        return json.dumps(res)
