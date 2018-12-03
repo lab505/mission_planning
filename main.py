4836cfab6bb80fee44b1ce1d51deb7ce1a13badf
cuijialiang@byted <cuijialiang@bytedance.com>
2018-12-03 17:12:02 +0800

    update command way

--- main.py
+++ main.py
@@ -16,12 +16,18 @@ subp_ = subparsers.add_parser("get_mission_planning_res")
 subp_ = subparsers.add_parser("get_status")
 kwargs = vars(parser.parse_args())
 
-opts = {
-    'create_mission': lambda: mission_planning.create_mission(kwargs['input'], mysql_conf_file=kwargs['mysql_conf_file']),
-    'add_data': lambda: mission_planning.add_data(kwargs['input'], mysql_conf_file=kwargs['mysql_conf_file']),
-    'get_main_ui_display': lambda: mission_planning.get_main_ui_display(mysql_conf_file=kwargs['mysql_conf_file']),
-    'get_mission_planning_res': lambda: mission_planning.get_mission_planning_res(mysql_conf_file=kwargs['mysql_conf_file']),
-    'get_status': lambda: mission_planning.get_status(mysql_conf_file=kwargs['mysql_conf_file']),
-}
+res = None
+if kwargs['command'] == 'create_mission':
+    res = mission_planning.create_mission(kwargs['input'], mysql_conf_file=kwargs['mysql_conf_file'])
+elif kwargs['command'] == 'add_data':
+    res = mission_planning.add_data(kwargs['input'], mysql_conf_file=kwargs['mysql_conf_file'])
+elif kwargs['command'] == 'get_main_ui_display':
+    res = mission_planning.get_main_ui_display(mysql_conf_file=kwargs['mysql_conf_file'])
+elif kwargs['command'] == 'get_mission_planning_res':
+    res = mission_planning.get_mission_planning_res(mysql_conf_file=kwargs['mysql_conf_file'])
+elif kwargs['command'] == 'get_status':
+    res = mission_planning.get_status(mysql_conf_file=kwargs['mysql_conf_file'])
+else:
+    res = 'unknown command'
 
-sys.stdout.write(str(opts[kwargs['command']]()) + '\n')
+print (str(res))
