0230233fff44aa80c062e24abbf46eb5aa00b88b
cuijialiang@byted <cuijialiang@bytedance.com>
2019-05-15 22:24:19 +0800

    move

--- preload_missions.py
+++ preload_missions.py
@@ -1,6 +1,45 @@
 # coding=utf8
 
 missions = {
+    '中尺度_区域1_0.1m_广域SAR': {
+        'application': '洪涝',
+        'aerocraft': '长航时固定翼无人机',
+        'cameras': '广域SAR',
+        'ground_resolution_m': 0.1,
+        'fly_direction': (1, 0),
+        'forward_overlap': 0.,
+        'sideway_overlap': 0.3,
+    },
+    '中尺度_区域1_0.1m_minisar': {
+        'application': '洪涝',
+        'aerocraft': '轻小型固定翼无人机',
+        'cameras': 'minisar',
+        'ground_resolution_m': 0.1,
+        'fly_direction': (1, 0),
+        'forward_overlap': 0.,
+        'sideway_overlap': 0.3,
+    },
+    '中尺度_区域1_0.1m_大视场立体测绘相机': {
+        'application': '洪涝',
+        'aerocraft': '轻小型固定翼无人机',
+        'cameras': '大视场立体测绘相机',
+        'ground_resolution_m': 0.1,
+        'fly_direction': (1, 0),
+        'forward_overlap': 0.6,
+        'sideway_overlap': 0.3,
+    },
+    '中尺度_区域1_0.1m_可见光': {
+        'application': '洪涝',
+        'aerocraft': '轻小型固定翼无人机',
+        'cameras': '轻型双波段相机',
+        'ground_resolution_m': 0.1,
+        'fly_direction': (1, 0),
+        'forward_overlap': 0.6,
+        'sideway_overlap': 0.3,
+    },
+
+
+    
     '大尺度_区域1_10m_高光谱':{
        'application': '生态',
         'aerocraft': '轻小型无人机(1)',
@@ -90,60 +129,5 @@ missions = {
         'fly_direction': (1, 0),
         'forward_overlap': 0.6,
         'sideway_overlap': 0.3, 
-    },
-    '中尺度_区域1_0.1m_广域SAR': {
-        'application': '洪涝',
-        'aerocraft': '长航时固定翼无人机',
-        'cameras': '广域SAR',
-        'ground_resolution_m': 0.1,
-        'fly_direction': (1, 0),
-        'forward_overlap': 0.,
-        'sideway_overlap': 0.3,
-    },
-    '中尺度_区域1_0.1m_minisar': {
-        'application': '洪涝',
-        'aerocraft': '轻小型固定翼无人机',
-        'cameras': 'minisar',
-        'ground_resolution_m': 0.1,
-        'fly_direction': (1, 0),
-        'forward_overlap': 0.,
-        'sideway_overlap': 0.3,
-    },
-    '中尺度_区域1_0.1m_大视场立体测绘相机': {
-        'application': '洪涝',
-        'aerocraft': '轻小型固定翼无人机',
-        'cameras': '大视场立体测绘相机',
-        'ground_resolution_m': 0.1,
-        'fly_direction': (1, 0),
-        'forward_overlap': 0.6,
-        'sideway_overlap': 0.3,
-    },
-    '中尺度_区域1_0.1m_可见光': {
-        'application': '洪涝',
-        'aerocraft': '轻小型固定翼无人机',
-        'cameras': '轻型双波段相机',
-        'ground_resolution_m': 0.1,
-        'fly_direction': (1, 0),
-        'forward_overlap': 0.6,
-        'sideway_overlap': 0.3,
-    },
-    
-    '小尺度_区域1_0.1m': {
-        'application': '反恐',
-        'aerocraft': '多旋翼无人机',
-        'cameras': 'Canon EOS 450D',
-        'ground_resolution_m': 0.1,
-        'fly_direction': (1, 0),
-        'forward_overlap': 0.6,
-        'sideway_overlap': 0.3,
-    },
-    '小尺度_区域1_1m': {
-        'application': '反恐',
-        'aerocraft': '轻小型固定翼无人机',
-        'cameras': '双波段视频吊舱',
-        'ground_resolution_m': 1,
-        'fly_direction': (1, 0),
-        'forward_overlap': 0.6,
-        'sideway_overlap': 0.3,
     },
 }
