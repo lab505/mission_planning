81719604d6177a06f1be805813d5dc193d0bf028
LQ <15651804306@pku.edu.cn>
2019-06-12 11:38:01 +0800

    modifypa

--- camera.py
+++ camera.py
@@ -4,24 +4,24 @@ cameras = {
     'minisar': {
         'type': 'sar',
         'name': 'minisar',
-        'R_min_m': 3000.,
-        'R_max_m': 4000.,
+        'R_min_m': 1000.,
+        'R_max_m': 6000.,
         'R_suggest_m': 3500.,
         'ground_resolution_m': .1,
         'right_look_angle_degrees': 45.,
         'range_beam_width_degrees': 6.,
     },
 
-    '广域SAR': {
-        'type': 'sar',
-        'name': '广域SAR',
-        'R_min_m': 3000.,
-        'R_max_m': 4000.,
-        'R_suggest_m': 4000.,
-        'ground_resolution_m': .1,
-        'right_look_angle_degrees': 45.,
-        'range_beam_width_degrees': 6.,
-    },
+    #'广域SAR': {
+       # 'type': 'sar',
+        #'name': '广域SAR',
+       # 'R_min_m': 3000.,
+       # 'R_max_m': 4000.,
+        #'R_suggest_m': 4000.,
+        #'ground_resolution_m': .1,
+        #'right_look_angle_degrees': 45.,
+       # 'range_beam_width_degrees': 6.,
+    #},
 
     '大视场立体测绘相机': {
         'type': 'camera',
@@ -66,7 +66,7 @@ cameras = {
         'name': '多光谱相机',
         'f_m': 6*0.001,
         'pixel_size_m': 3.75*0.001*0.001,
-        'pixel_num_x': 1920,
-        'pixel_num_y': 1080,
+        'pixel_num_x': 1280,
+        'pixel_num_y': 960,
     },
 }
